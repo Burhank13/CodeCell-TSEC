ed6c5663e27fc08928122a2c256cb2600371af62
Burhank13 <54506431+Burhank13@users.noreply.github.com>
2020-01-15 23:53:49 +0530

    cube it up

--- cubeitup.cpp
+++ cubeitup.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int cit()
 {
     long long int t,dig,dig1;
-    long long int cube=0, num=0, sum1=0, sum=0, n;
+    long long int cube=0, num=0, sum1=1, sum=0, n;
     cout<<"enter no. of testcases";
     cin>>t;
     while(t!=0)
@@ -16,29 +16,16 @@ int cit()
         {
             cube = i * i * i;
             sum = 0;
-            while (cube != 0) {
+            while (cube != 0)
+            {
                 dig = cube % 10;
                 cube = cube / 10;
                 sum = sum + dig;
             }
             num += sum;
         }
-        num %= 1000007;
-        while(num>9)
-        {
-            sum1=0;
-            while(num!=0)
-            {
-                dig1=num%10;
-                num=num/10;
-                sum1 += dig1;
-            }
-            if(sum1>9)
-            {
-                num=sum1%1000007;
-            }
-        }
-        cout<<sum1;
+        cout<<num;
+        sum1=sum1*num%1000007;
     t--;
     }
     return 0;
